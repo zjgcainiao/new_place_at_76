cc1e2056a805d1762603959835b4cfb055cf324b
Stephen Wang <holleratme420@gmail.com>
2023-04-06 17:09:28 -0500

    remove .ipynb files

--- prolube76site/homepageapp/forms.py
+++ prolube76site/homepageapp/forms.py
@@ -1,19 +1,21 @@
 from django import forms
-from homepageapp.models import CustomersNewSQL01Model, VehiclesNewSQL01Model , RepairOrdersNewSQL01Model
+from homepageapp.models import CustomersNewSQL02Model, VehiclesNewSQL02Model , RepairOrdersNewSQL02Model
 
 class CustomerModelForm(forms.ModelForm):
     class Meta:
-        model = CustomersNewSQL01Model
+        model = CustomersNewSQL02Model
         fields = ['customer_id', 'customer_first_name','customer_last_name', 'customer_does_allow_SMS',]
 
 class VehicleModelForm(forms.ModelForm):
     class Meta:
-        model = VehiclesNewSQL01Model
+        model = VehiclesNewSQL02Model
         fields = ['vehicle_id','vehicle_cust_id','VIN_number','vehicle_memo_01','vehicle_odometer_1']
         # exclude = ('created_at', 'updated_at',)
 
 # Create your views here.
 class RepairOrderModelForm(forms.ModelForm):
     class Meta:
-        model = RepairOrdersNewSQL01Model
-        fields = ['repair_order_id','repair_order_created_as_estimate','repair_order_snapshot_order_total_amount']
+        model = RepairOrdersNewSQL02Model
+        fields = ['repair_order_id','repair_order_customer',
+                  'repair_order_phase',
+                  'repair_order_created_as_estimate','repair_order_snapshot_order_total_amount']
