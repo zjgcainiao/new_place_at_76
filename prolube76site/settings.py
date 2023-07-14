765fd664b681f53abeae29203bee26eb544d2def
Stephen Wang <holleratme420@gmail.com>
2023-07-14 02:38:17 -0700

    delete all foldes inside the assets folder

--- prolube76site/settings.py
+++ prolube76site/settings.py
@@ -330,23 +330,6 @@ USE_TZ = False # turned the USE_TZ to False to avoid fetching data error when fe
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-<<<<<<< HEAD:prolube76site/prolube76site/settings.py
-# STATIC_URL = 'static/'
-
-# using storage bucket to host static files
-# STATIC_URL = 'https://storage.googleapis.com/2023_new_prolube76site/static_files'
-STATIC_URL = 'https://storage.googleapis.com/{}/static_files/'.format(GS_BUCKET_NAME)
-
-
-STATIC_ROOT = BASE_DIR / 'assets'
-
-STATICFILES_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
-
-# set up the base folder to host static files in 
-# "76prolubeplus.com/prolube76site/static"
-# static files include javacscript
-=======
->>>>>>> main:prolube76site/settings.py
 
 # STATIC_URL = 'static/'
 
