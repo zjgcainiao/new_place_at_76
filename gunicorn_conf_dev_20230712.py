4350787c1d2ed0aef397ea35d1ca9b567c38f954
Stephen Wang <holleratme420@gmail.com>
2023-07-13 23:24:15 -0700

    remove .deployment, deploy.sh that causes deployment to azure app service failing during deployment. 2. modified the accesslog and pidfile config in gunicorn.conf file.

--- gunicorn_conf_dev_20230712.py
+++ gunicorn_conf_dev_20230712.py
@@ -8,9 +8,9 @@ worker = 4
 # work class
 work_class = 'sync'
 # Write access and error info to /var/log
-accesslog = errorlog = '../gunicorn/gunicorn-logfile-dev.log'
+accesslog = errorlog = './gunicorn/gunicorn-logfile-dev.log'
 # PID file so you can easily fetch process ID
-pidfile = "../gunicorn/gunicorn-dev.pid"
+pidfile = "./gunicorn/gunicorn-dev.pid"
 # Restart workers when code changes (development only!)
 reload = True
 #set to "*" to disable checking of Front-end IPs; useful when you don't know in advance the ip address of front-end, but you still trust the environment
@@ -18,5 +18,5 @@ proxy_protocol = True
 proxy_allow_ips = '*'
 daemon = True
 # use this feature to allow the terminal to display the fully resolved conf.py file.
-# print_config = False  # True
-print_config = True
+print_config = False  # True
+# print_config = True
