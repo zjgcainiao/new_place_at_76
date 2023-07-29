e8ad77670f2baab4c4bc1f1a5b723162f58e1173
Stephen Wang <holleratme420@gmail.com>
2023-07-29 12:55:38 -0700

    using setup-python caching example

--- homepageapp/tests.py
+++ homepageapp/tests.py
@@ -1,20 +1,22 @@
 import datetime
 
+from django.test import Client
 from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse
 
-from .models import Question
+from homepageapp.models import RepairOrdersNewSQL02Model, CustomersNewSQL02Model, AddressesNewSQL02Model, 
 
-
-def create_question(question_text, days):
+def create_customer(question_text, days):
+# def create_question(question_text, days):
+    
         """
         Create a question with the given `question_text` and published the
         given number of `days` offset to now (negative for questions published
         in the past, positive for questions that have yet to be published).
         """
         time = timezone.now() + datetime.timedelta(days=days)
-        return Question.objects.create(question_text=question_text, pub_date=time)
+        return CustomersNewSQL02Model.objects.create(question_text=question_text, pub_date=time)
 
 
 class QuestionIndexViewTests(TestCase):
--- prolube76site/setting_select/staging.py
+++ prolube76site/setting_select/staging.py
@@ -6,9 +6,8 @@ DEBUG = True
 
 # on the development, we explicitly disable the https connection due to an error received
 # 2022-11-13
-SECURE_SSL_REDIRECT = False
-CSRF_COOKIE_SECURE = False
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = False
-SECURE_SSL_REDIRECT = False
 SECURE_BROWSER_XSS_FILTER = False
 SECURE_CONTENT_TYPE_NOSNIFF = True
