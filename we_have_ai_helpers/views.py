260c4c03b2c24d0d7571845c2be05c891ba1a9f4
Stephen Wang <holleratme420@gmail.com>
2023-08-01 04:55:22 -0700

    modify the pdf scraping pilot function so that empty links will be skipped

--- we_have_ai_helpers/views.py
+++ we_have_ai_helpers/views.py
@@ -5,8 +5,8 @@ from we_have_ai_helpers.webscraper import scrape_and_download_pdfs
 from django.core.files.storage import default_storage
 
 def list_pdfs(request):
-    # Get a list of all pdf files
-    pdf_files = [f for f in default_storage.listdir('scraped_pdfs')[1]]
+    # Get a list of all pdf files. add if f to eclude empty ones
+    pdf_files = [default_storage.url('scraped_pdfs/' + f) for f in default_storage.listdir('scraped_pdfs')[1] if f]
 
     # Render the 'list_pdfs.html' template and pass the pdf files as context
     return render(request, 'we_have_ai_helpers/10_list_federal_reserve_loan_officer_survey_data.html', {'pdf_files': pdf_files})
