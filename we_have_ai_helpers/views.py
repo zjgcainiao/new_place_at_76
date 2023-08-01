fa7c6b6ed6f48b42a383c0ba13ac29f7e7307db7
Stephen Wang <holleratme420@gmail.com>
2023-07-31 20:57:34 -0700

    add beautifulsoup4 in the requirements.txt. remove jinja2==3.1.2

--- we_have_ai_helpers/views.py
+++ we_have_ai_helpers/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+import os
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
-# Create your views here.
+
+def scrape_and_download_pdfs():
+    base_url = "https://www.federalreserve.gov/data/sloos.htm"
+
+    # Send a GET request
+    response = requests.get(base_url)
+
+    # If the GET request is successful, the status code will be 200
+    if response.status_code == 200:
+        # Get the content of the response
+        page_content = response.content
+        # Create a BeautifulSoup object and specify the parser
+        soup = BeautifulSoup(page_content, 'html.parser')
+        
+        # Find all pdf links
+        pdf_links = soup.select("a[href$='.pdf']")
+
+        # Filter pdfs of 2023
+        pdf_links = [pdf for pdf in pdf_links if '2023' in pdf['href']]
+
+        for link in pdf_links:
+            # Name the pdf files using the last portion of each link which are unique in this case
+            filename = os.path.join("pdfs", link['href'].split("/")[-1])
+            # Download the pdfs to the specified location
+            with open(filename, 'wb') as f:
+                f.write(requests.get(urljoin(base_url, link['href'])).content)
