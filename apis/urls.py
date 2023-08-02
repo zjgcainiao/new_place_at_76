617a1a20641d81289f4f2943216993ad59624452
Stephen Wang <holleratme420@gmail.com>
2023-08-01 20:32:36 -0700

    update urls.py in apis with router.urls

--- apis/urls.py
+++ apis/urls.py
@@ -5,15 +5,13 @@ from apis.views import RepairOrderViewSet, LineItemsViewSet, TextMessagesViewSet
 
 app_name = 'apis'
 
-
-
-
 router = DefaultRouter()
 router.register(r'repair_orders', RepairOrderViewSet)
 router.register(r'line_items', LineItemsViewSet)
 router.register(r'text_messages', TextMessagesViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('customers/', views.customer_api, name='customers-api'),  # apis/cust
     path('ros/', views.repairorders_api, name='repairorders-api'),  # apis/cust`
     # path('apis/repairorders', views.RepairOrderModelForm, name='api-repair-order'),
