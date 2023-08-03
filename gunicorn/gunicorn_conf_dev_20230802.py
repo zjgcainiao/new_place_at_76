e82fb2dc72078f3e7b67acc4b660ddefb0376d9f
Stephen Wang <holleratme420@gmail.com>
2023-08-02 20:05:52 -0700

    modify the gunicorn conf: 1. use the full path for access_log, pid, or error_log

--- gunicorn/gunicorn_conf_dev_20230802.py
+++ gunicorn/gunicorn_conf_dev_20230802.py
@@ -1,16 +1,16 @@
 wsgi_app = 'prolube76site.wsgi:application'
-pythonpath = '../prolube76site'
+pythonpath = '/Users/stephenwang/new_76prolubeplus.com'
 # command = '/Users/stephenwang/my_venv/bin/gunicorn'
 loglevel = 'debug'
 # loglevel = 'info'
-bind = '0.0.0.0:8080'
+bind = '0.0.0.0:8000'
 worker = 4
 # work class
 work_class = 'sync'
 # Write access and error info to /var/log
-accesslog = errorlog = '../gunicorn/gunicorn-logfile-dev.log'
+accesslog = errorlog = '/Users/stephenwang/new_76prolubeplus.com/gunicorn/gunicorn-logfile-dev.log'
 # PID file so you can easily fetch process ID
-pidfile = "../gunicorn/gunicorn-dev.pid"
+pidfile = "/Users/stephenwang/new_76prolubeplus.com/gunicorn/gunicorn-dev.pid"
 # Restart workers when code changes (development only!)
 reload = True
 # set to "*" to disable checking of Front-end IPs; useful when you don't know in advance the ip address of front-end, 
@@ -19,5 +19,5 @@ proxy_protocol = True
 proxy_allow_ips = '*'
 daemon = True
 # use this feature to allow the terminal to display the fully resolved conf.py file.
-print_config = False  # True
+print_config = False
 # print_config = True
