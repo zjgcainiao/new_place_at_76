ba7d7c7d51ccb0c1b9d3b05a2f831da1fb063332
Stephen Wang <holleratme420@gmail.com>
2023-09-30 13:51:24 -0700

    added initial shift and TimeClock models in the shift_management.models

--- shift_management/models.py
+++ shift_management/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from internal_users.models import InternalUser
+from talent_management.models import TalentsModel
 
-# Create your models here.
+
+class Shift(models.Model):
+    employee = models.ForeignKey(TalentsModel, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+
+class TimeClock(models.Model):
+    employee = models.ForeignKey(TalentsModel, on_delete=models.CASCADE)
+    clock_in_time = models.DateTimeField()
+    clock_out_time = models.DateTimeField(null=True, blank=True)
+    hours_worked = models.DurationField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.clock_in_time and self.clock_out_time:
+            self.hours_worked = self.clock_out_time - self.clock_in_time
+        super().save(*args, **kwargs)
