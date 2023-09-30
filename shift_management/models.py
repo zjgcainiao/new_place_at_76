becfbc955c65263ffc9c478caa0afe0bf89c9c44
Stephen Wang <holleratme420@gmail.com>
2023-09-30 14:21:54 -0700

    1. added holidays in requirements.txt 2. added initial models in shift_management app 3. updated the messages section button css_class from close to btn-close

--- shift_management/models.py
+++ shift_management/models.py
@@ -4,13 +4,26 @@ from talent_management.models import TalentsModel
 
 
 class Shift(models.Model):
-    employee = models.ForeignKey(TalentsModel, on_delete=models.CASCADE)
+    shift_id = models.AutoField(primary_key=True)
+    talent = models.ForeignKey(
+        TalentsModel, on_delete=models.CASCADE, related_name='shifts')
+    internal_user = models.ForeignKey(
+        InternalUser, on_delete=models.CASCADE, related_name="shift_changes")
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+    scheduled_hours = models.DurationField()
+    note = models.TextField(blank=True)
+
+    class Meta:
+        db_table = 'shfts_new_03'
 
 
 class TimeClock(models.Model):
-    employee = models.ForeignKey(TalentsModel, on_delete=models.CASCADE)
+    timeclock_id = models.AutoField(primary_key=True)
+    talent = models.ForeignKey(
+        TalentsModel, on_delete=models.CASCADE, related_name='timeclocks')
+    internal_user = models.ForeignKey(
+        InternalUser, on_delete=models.CASCADE, related_name="timeclock_changes")
     clock_in_time = models.DateTimeField()
     clock_out_time = models.DateTimeField(null=True, blank=True)
     hours_worked = models.DurationField(null=True, blank=True)
