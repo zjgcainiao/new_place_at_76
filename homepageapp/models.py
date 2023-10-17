b30a0d7fa0f544f6735ae9dbe0236794daaabba1
Stephen Wang <holleratme420@gmail.com>
2023-10-16 17:05:32 -0700

    added Vendors model in homepageapp

--- homepageapp/models.py
+++ homepageapp/models.py
@@ -10,6 +10,7 @@ from dotenv import load_dotenv
 from internal_users.models import InternalUser
 import re
 from apis.api_vendor_urls import NHTSA_API_URL
+from core_operations.models import FormattedPhoneNumberField
 # documentation for py-mssql is https://pymssql.readthedocs.io/en/stable/pymssql_examples.html#basic-features-strict-db-api-compliance
 # import pymssql
 
@@ -62,6 +63,39 @@ class AccountClassModel(models.Model):
         ordering = ["-account_class_id"]
         verbose_name = 'accountclass'
         verbose_name_plural = 'accountclasses'
+
+# 2023-10-16 added new model Vendors (for vehicles)
+class Vendors (models.Model):
+    vendor_id  = models.AutoField(primary_key=True)
+    vendor_name = models.CharField(max_length=50, null=True, verbose_name='Business Name')
+    vendor_contact_persons = models.CharField(max_length=50, null=True, verbose_name='Contact Person (i.e. Kenny)')
+    vendor_contact_phone_number = FormattedPhoneNumberField(null=True)
+    vendor_comment = models.CharField(max_length=200, null=True)
+    vendor_contact_email_address = models.CharField(max_length=50, null=True)
+    vendor_is_active = models.BooleanField(
+        default=True, null=True, blank=True)
+    vendor_email_verified = models.BooleanField(
+        default=False, null=True, blank=True)
+    vendor_last_email_verified_at = models.DateTimeField(blank=True, null=True)
+    vendor_email_verified_type = models.CharField(max_length=200, null=True)
+    vendor_code = models.CharField(max_length=15, null=True)
+    vendor_limit = models.CharField(max_length=15, null=True)
+    vendor_terms = models.CharField(max_length=50, null=True)
+    vendor_account_class = models.CharField(max_length=15, null=True)
+    vendor_type = models.IntegerField(blank=True, null=True)
+    vendor_catelog_link = models.IntegerField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(
+        auto_now=True, null=True)
+    created_by = models.ForeignKey(
+        InternalUser, related_name='vendors_created_by', on_delete=models.SET_NULL, null=True, blank=True)
+    updated_by = models.ForeignKey(
+        InternalUser, related_name='vendors_updated_by', on_delete=models.SET_NULL, null=True, blank=True)
+    class Meta:
+        db_table = 'vendors_new_03'
+        ordering = ["-vendor_id"]
+        verbose_name = 'vendor'
+        verbose_name_plural = 'vendors'
 
 
 class InvoiceStatusModel(models.Model):
--- prolube76site/settings.py
+++ prolube76site/settings.py
@@ -417,7 +417,7 @@ if server:
 
     # use the Microsoft provided MSSQL DRIVER for Django
     DATABASES = {
-        "default": {
+        "default_actual": {
             "ENGINE": "mssql",
             "NAME": databaseName,
             "USER": user,
@@ -428,7 +428,7 @@ if server:
                         "extra_params": "TrustServerCertificate=yes;Encrypt=no;"
                         },
         },
-        'demo': {
+        'default': {
             'ENGINE': 'mssql',
             "NAME": demoDatabaseName,
             "USER": user,
