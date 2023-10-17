01e929915ae49eb56e74e48f9f245f70bf8ac08b
Stephen Wang <holleratme420@gmail.com>
2023-10-16 19:02:57 -0700

    1. fixed the catelog type. the correct one is catalogLinks. 2. add data reading scripts in read-data-into-models-script.py

--- homepageapp/models.py
+++ homepageapp/models.py
@@ -144,37 +144,39 @@ class CategoryModel(models.Model):
             self.created_by = self.modified_by
         super().save(*args, **kwargs)
 
-class CatelogLinks(models.Model):
-    catelog_link_id = models.AutoField(primary_key=True)
-    catelog_link_file_used  = models.CharField(
-        max_length=50, blank=True, null=True)
-    catelog_vendor_display_name =models.CharField(
-        max_length=50, blank=True, null=True)
-    catelog_version = models.CharField(
-        max_length=10, blank=True, null=True)
-    catelog_auth_code= models.CharField(
-        max_length=50, blank=True, null=True)
+# this model stores specs data related to vendors which provide catelog links. VendorLink stores the actual format of the link used.
+# could be outdated for future use.
 
+class CatalogLinks(models.Model):
+    catalog_link_id = models.AutoField(primary_key=True)
+    catalog_link_file_used = models.CharField(
+        max_length=50, blank=True, null=True)
+    catalog_vendor_display_name =models.CharField(
+        max_length=50, blank=True, null=True)
+    catalog_version = models.CharField(
+        max_length=10, blank=True, null=True)
+    catalog_auth_code = models.CharField(
+        max_length=50, blank=True, null=True)
 
     updated_at = models.DateTimeField(auto_now=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by  = models.ForeignKey(
+    created_by = models.ForeignKey(
         InternalUser, related_name='catelog_links_created_by', on_delete=models.SET_NULL, null=True, blank=True)
     updated_by = models.ForeignKey(
         InternalUser, related_name='catelog_links_updated_by', on_delete=models.SET_NULL, null=True, blank=True)
 
     class Meta:
-        db_table = 'catelog_links_new_03'
-        ordering = ['-catelog_link_id']
-        verbose_name = '(Vendor) Catelog Link'
-        verbose_name_plural = '(Vendor) Catelog Links'
+        db_table = 'catalog_links_new_03'
+        ordering = ['-catalog_link_id']
+        verbose_name = '(Vendor) Catalog Link'
+        verbose_name_plural = '(Vendor) Catalog Links'
+
     def save(self, *args, **kwargs):
         if self._state.adding:
             self.created_by = self.updated_by
         super().save(*args, **kwargs)
 
-
-
+# 2023-10-16 added new model VendorType. It defines types of vehicle related whole sale vendors: tire shop, repair shop, dealerhip
 class VendorTypes(models.Model):
     vendor_type_id = models.AutoField(primary_key=True)
     vendor_type_name = models.CharField(max_length=100, null=True, verbose_name='Vendor Type Name')
