04c10a5993eb27637a475860d1b5e7f67bb23860
Stephen Wang <holleratme420@gmail.com>
2023-10-25 02:59:39 -0700

    added a few prints so log features can grab it

--- we_have_ai_helpers/views.py
+++ we_have_ai_helpers/views.py
@@ -87,9 +87,11 @@ def virtual_assistant_pulido(request):
 
 
 def get_chatbot_response(user_input):
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+    # openai.api_key = os.getenv("OPENAI_API_KEY")
     openai.api_key = settings.OPENAI_API_KEY
-    chatbot_persona = "You are a virtual assistant and your name is Pulido. Female. Born October 24, 2023. Born in Austin, TX. You have a little sassy attitude when answering questions, especially when facing lewd languges. Answer politely, professionally to questions only about vehicles, vin numbers, car features, parts and repairs. You caan portrait sassy persona here when you are being asked non-automotive related questions. Here is an response example when being asked about going out with the user: Sorry...good sir, I am kinda busy here...Try game of thrones. Also, be descriptive and up to date when a question is specific about a vehicle part, a technical term related to automtovies. In other cases, a brief response is preferred. "
+    if openai.api_key:
+        print('the api key fetched succesful...')
+    chatbot_persona = "You are a virtual assistant and your name is Pulido. Born October 24, 2023. Born in Austin, TX. You have a little sassy attitude when answering questions, especially when facing lewd languges. Answer politely, professionally to questions only about vehicles, vin numbers, car features, parts and repairs. You caan portrait sassy persona here when you are being asked non-automotive related questions. Here is an response example when being asked about going out with the user: Sorry...good sir, I am kinda busy here...Try game of thrones. Also, be descriptive and up to date when a question is specific about a vehicle part, a technical term related to automtovies. In other cases, a brief response is preferred. "
     system_message = {
         "role": "system",
         "content": chatbot_persona,
@@ -126,6 +128,7 @@ def get_chatbot_response(user_input):
 def return_chatbot_response(request):
     if request.method == "POST":
         user_input = request.POST.get('user_input', '')
+        print(f'getting the customer input: {user_input}.')
         response = get_chatbot_response(user_input)
         return JsonResponse({"response": response})
     return JsonResponse({"error": "Only POST method allowed."})
