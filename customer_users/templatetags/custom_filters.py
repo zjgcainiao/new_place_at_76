65521239e7332f1537343f5fdc50b953fa3746af
Stephen Wang <holleratme420@gmail.com>
2023-10-25 01:00:58 -0700

    added a custom filter function is_instance_of(value, args) in customers_users templatetags folder

--- customer_users/templatetags/custom_filters.py
+++ customer_users/templatetags/custom_filters.py
@@ -7,3 +7,9 @@ register = template.Library()
 @register.filter(name='has_group')
 def has_group(user, group_name):
     return user.groups.filter(name=group_name).exists()
+
+
+@register.filter(name='is_instance_of')
+def is_instance_of(value, arg):
+    model_class = globals().get(arg)
+    return isinstance(value, model_class)
