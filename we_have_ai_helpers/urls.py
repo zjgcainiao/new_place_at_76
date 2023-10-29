9d36774cda14965073aa9c1effb04f039bd3b819
Stephen Wang <holleratme420@gmail.com>
2023-10-28 22:29:39 -0500

    added Roboto font in the Chat UI

--- we_have_ai_helpers/urls.py
+++ we_have_ai_helpers/urls.py
@@ -6,5 +6,5 @@ app_name = "we_have_ai_helpers"
 urlpatterns = [
     # sloos federal quarterly agency reports
     path('sloos_pdfs/', views.list_pdfs, name='list_sloos_pdfs'),
-    path('chatbot/', views.return_chatbot_response, name='chatbot_response'),
+    path('chatbot/', views.return_simple_chatbot_response, name='chatbot_response'),
 ]
--- we_have_ai_helpers/views.py
+++ we_have_ai_helpers/views.py
@@ -129,8 +129,7 @@ def get_chatbot_response(user_input):
 
 # 2023-10-24 current chatbot
 
-
-def return_chatbot_response(request):
+def return_simple_chatbot_response(request):
     logger = logging.getLogger('external_api')
     logger.info(f'Starting the chatbot view function.....')
     if request.method == "POST":
@@ -138,6 +137,6 @@ def return_chatbot_response(request):
         print(f'getting the customer input: {user_input}.')
         response = get_chatbot_response(user_input)
         logger.info(f'getting reponse from openAI.com: {response}')
-        print(f'getting reponse from openAI.com: {response}')
+        # print(f'getting reponse from openAI.com: {response}')
         return JsonResponse({"response": response})
     return JsonResponse({"error": "Only POST method allowed."})
